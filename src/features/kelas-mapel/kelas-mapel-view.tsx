c215cdd9d516ff9c7a64b765194f8c8c1df701e3
maulana <maulanaabdullana123@gmail.com>
2025-06-30 09:52:37 +0700

    first commit

--- src/features/kelas-mapel/kelas-mapel-view.tsx
+++ src/features/kelas-mapel/kelas-mapel-view.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { useEffect, useState } from 'react';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
